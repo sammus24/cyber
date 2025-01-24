f8be039dbf8d72cc9c47a09af466b7ad32a05ac2
cybernirav <61507457+cybernirav@users.noreply.github.com>
2025-01-24 16:21:33 -0500

    Delete assets directory

--- assets/js/main.js
+++
@@ -1,9 +0,0 @@
-function login() {
-    const username = prompt("Enter your username:");
-    if (!username) {
-        alert("You must enter a username!");
-        return;
-    }
-    localStorage.setItem("username", username);
-    alert(`Welcome, ${username}!`);
-}
